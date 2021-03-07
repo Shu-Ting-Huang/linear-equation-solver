1a11af630034d72cbe54c2f9377708bc91beffbf
Shu-Ting Huang <sthuang890702@gmail.com>
2021-03-07 13:40:11 -0500

    Add matrix2latex

--- frontend.py
+++ frontend.py
@@ -17,3 +17,18 @@ def frac2latex_omit1(x):
         return '\\frac{' + str(x.p) + "}{" + str(x.q) + "}"
     else:
         return '-\\frac{' + str(-x.p) + "}{" + str(x.q) + "}"
+
+def matrix2latex(A, bar_config=''): #bar_config indicate where to put vertical bar. Input format like "[ccc|c]"
+    (m,n) = A.shape
+    result = '\\begin{patrix}' + bar_config + '\n'
+    for i in range(m): #i row through rows of A
+        for j in range(n-1): #j row through columns of A except the last one
+            result += (frac2latex(A[i,j]) + ' & ')
+
+        #dealing with the last column
+        if i != m-1: #not the last row
+            result += (frac2latex(A[i,n-1]) + '\\\\[4pt]\n')
+        else: #the last row
+            result += (frac2latex(A[i,n-1]) + '\n')
+    result += '\\end{pmatrix}\n'
+    return result
